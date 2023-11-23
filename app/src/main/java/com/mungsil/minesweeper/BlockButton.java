c0314cb5f2208ed94c22446871aa3fd98db4d6dd
mungsil <thddms2009@naver.com>
2023-11-23 21:59:02 +0900

    2023.11.23

--- app/src/main/java/com/mungsil/minesweeper/BlockButton.java
+++ app/src/main/java/com/mungsil/minesweeper/BlockButton.java
@@ -5,26 +5,20 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.Button;
 import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 @SuppressLint("AppCompatCustomView")
 public class BlockButton extends Button {
-
-
     private int x;
     private int y;
-    //지뢰니? true or false로 대답하렴.
     private boolean mine;
-    //flag가 꽃혔니? true or false로 대답하렴.
     private boolean flag;
     int neighborMines;
     static int flags=10;
-    static int blocks=81;
+    static int blocks=0;
 
-
-    //레이아웃 생성자 안으로 이동
     public BlockButton(Context context, int x, int y) {
-        //매개변수, 부모의 생성자 호출, 필드 초기화
         super(context);
         mine = false;
         flag = false;
@@ -32,10 +26,9 @@ public class BlockButton extends Button {
         this.x = x;
         this.y = y;
 
-        //남은 블록 수 증가
+        //총 블록 수 증가
         blocks = blocks + 1;
 
-        //LayoutParameter 설정
         TableRow.LayoutParams layoutParams =
                 new TableRow.LayoutParams(
                         TableRow.LayoutParams.WRAP_CONTENT,
@@ -45,35 +38,34 @@ public class BlockButton extends Button {
         super.setLayoutParams(layoutParams);
     }
 
-    // 깃발 꽃기 , 해제 메소드
-    //버튼안에 글자넣기: setText
     public void toggleFlag() {
-        if (flag == false) {
+
+           if (flag == false) {
             if (flags > 0) {
                 flag = true;
                 flags -= 1;
-
+                blocks--;
                 setText("+");
+
             }
 
         }else {
             flag = false;
             flags += 1;
+            blocks++;
             setText("");
+
         }
     }
 
-    // 블록을 여는 메소드
     public boolean breakBlock() {
         this.setEnabled(false);
-        System.out.println("블록 열어!");
         blocks--;
         if (mine == true) {
             setText("*");
             return true;
         }else {
             setText(neighborMines+"");
-            //열린 블록으로 표시하라는데? 근데 그게 머임;
             return false;
         }
     }
@@ -92,9 +84,6 @@ public class BlockButton extends Button {
     }
     public int getBlock_Y() {
         return this.y;
-    }
-    public boolean getMine() {
-        return this.mine;
     }
     public boolean isFlag() {
         return flag;
--- app/src/main/java/com/mungsil/minesweeper/MainActivity.java
+++ app/src/main/java/com/mungsil/minesweeper/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mungsil.minesweeper;
 
+import static com.google.android.material.internal.ViewUtils.getContentView;
+import static com.mungsil.minesweeper.BlockButton.blocks;
 import static com.mungsil.minesweeper.R.*;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,51 +13,51 @@ import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    static int mine = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
 
-        TableLayout tableLayout = (TableLayout) findViewById(id.tableLayout);
-        TextView mineText = findViewById(id.mineNumText);
-        Button[][] buttons = new BlockButton[9][9];
+        //지뢰 랜덤 배치
 
-        int mine = 0;
-        int width = 9;
-        int height = 9;
-        int[][] map = new int[width][height];
+        int[][] map = new int[9][9];
+        int[] nx = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int[] ny = {0, 1, 1, 1, 0, -1, -1, -1};
 
         while (mine < 10) {
-            int x = (int) (Math.random() * width);
-            int y = (int) (Math.random() * height);
-            if (map[y][x] == -1) { //지뢰 중복 시 재시도
+            int x = (int) (Math.random() * 9);
+            int y = (int) (Math.random() * 9);
+            if (map[y][x] == -1) {
                 continue;
             } else {
-                map[y][x] = -1; //지뢰 생성
+                map[y][x] = -1; //지뢰 표시
 
-                int[] nx = {-1, -1, 0, 1, 1, 1, 0, -1};
-                int[] ny = {0, 1, 1, 1, 0, -1, -1, -1};
                 for (int i = 0; i < 8; i++) {
-                    int newX = x + nx[i];
-                    int newY = y + ny[i];
+                    int neighborX = x + nx[i];
+                    int neighborY = y + ny[i];
 
-                    if (newX >= 0 && newX < width && newY >= 0 && newY < height && map[newY][newX] != -1) {
-                        map[newY][newX] += 1;
+                    if ( neighborX>= 0 && neighborX < 9 && neighborY >= 0 && neighborY < 9 && map[neighborY][neighborX] != -1) {
+                        map[neighborY][neighborX] += 1;
                     }
                 }
             }
             mine++;
         }
-        //
+
+        TextView mineText = findViewById(id.mineNumText);
         mineText.setText("10");
+
+        // 동적으로 버튼 생성
+        TableLayout tableLayout = (TableLayout) findViewById(id.tableLayout);
         TableRow tableRow[]=new TableRow[9];
+        Button[][] buttons = new BlockButton[9][9];
+
         for (int i = 0; i < 9; i++) {
             tableRow[i] = new TableRow(this);
                         tableLayout.addView(tableRow[i]);
@@ -62,67 +65,81 @@ public class MainActivity extends AppCompatActivity {
 
                         for (int j = 0; j < 9; j++) {
                             buttons[i][j] = new BlockButton(this, i, j);
-
                             tableRow[i].addView(buttons[i][j]);
                             if (map[i][j] == -1) {
                                 ((BlockButton)buttons[i][j]).setMine(true);
-                                System.out.println("buttons["+i+"]["+j+"] = "+ ((BlockButton)buttons[i][j]).getMine());
                             } else {
                                 ((BlockButton)buttons[i][j]).setNeighborMines(map[i][j]);
                             }
                             buttons[i][j].setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
-                                    // 깃발 꽃기
-                                if (toggleBtn.isChecked()) {
-                                    ((BlockButton)view).toggleFlag();
 
-                                }
-                                // 블록 열기
-                                else {
-                                    if (!((BlockButton) view).isFlag()) {
-                                        boolean isMine = ((BlockButton) view).breakBlock();
+                                    if (toggleBtn.isChecked()) {
+                                        ((BlockButton)view).toggleFlag();
+                                    }
 
-                                        if (isMine) {
-                                            for (int i = 0; i < 9; i++) {
-                                                for (int j = 0; j < 9; j++) {
-                                                    ((BlockButton)buttons[i][j]).breakBlock();
+                                    // 블록 열기
+                                    else if(!toggleBtn.isChecked()) {
+
+                                        if (!((BlockButton) view).isFlag()) {
+                                            boolean isMine = ((BlockButton) view).breakBlock();
+
+                                            if (isMine) {
+                                                for (int i = 0; i < 9; i++) {
+                                                    for (int j = 0; j < 9; j++) {
+                                                        ((BlockButton)buttons[i][j]).breakBlock();
+                                                        buttons[i][j].setEnabled(false);
+                                                    }
+                                                }
+                                                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                                                dialog.setTitle("게임종료");
+                                                dialog.setMessage("지뢰를 발견하였습니다");
+                                                dialog.show();
+
+                                            } else {
+
+                                                int neighborMines = ((BlockButton) view).getNeighborMines();
+
+                                                // 주변 지뢰 수가 =0인 블록이 있을 경우 해당 블록의 주변 블록 열기
+                                                if (neighborMines == 0) {
+
+                                                    int x = ((BlockButton) view).getBlock_X();
+                                                    int y = ((BlockButton) view).getBlock_Y();
+
+                                                    recursionBreak(x,y);
                                                 }
                                             }
-                                            // AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                                            Toast.makeText(MainActivity.this, "게임 종료", Toast.LENGTH_SHORT).show();
-                                        } else {
-                                            int neighborMines = ((BlockButton) view).getNeighborMines();
-                                            // 주변 지뢰 수가 >0 인 경우
-                                            // 머 해줄게 있나? 그냥...
-
-                                            // 주변 지뢰 수가 =0 인 경우 재귀 호출로 주변의 모든 블록 열기
-                                            if (neighborMines == 0) {
-                                                int[] nx = {-1, -1, 0, 1, 1, 1, 0, -1};
-                                                int[] ny = {0, 1, 1, 1, 0, -1, -1, -1};
-                                                int x = ((BlockButton) view).getBlock_X();
-                                                int y = ((BlockButton) view).getBlock_Y();
-
-                                                for (int i = 0; i < 8; i++) {
-                                                    if((x-nx[i])>-1 && (x-nx[i])<9 && (y-ny[i])>-1 && (y-ny[i])<9){
-                                                    ((BlockButton) buttons[x-nx[i]][y-ny[i]]).breakBlock();
-                                                }}
-
-                                            }
-
-
-
                                         }
                                     }
-                                }
-                                //남은 블록이 없으면 WIN
-                                    if (BlockButton.blocks == 0) {
-                                        // AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                                        Toast.makeText(MainActivity.this, "니가이김", Toast.LENGTH_SHORT).show();
+                                    //남은 블록이 없으면 WIN
+                                    if (blocks == 0) {
+                                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                                        dialog.setTitle("우승");
+                                        dialog.setMessage("게임 이겼어요");
+                                        dialog.show();
                                     }
 
                             }
-                });
+
+
+                                public void recursionBreak(int x, int y) {
+                                        for (int i = 0; i < 8; i++) {
+                                            int neighborX = x - nx[i];
+                                            int neighborY = y - ny[i];
+
+                                            if (neighborX >= 0 && neighborX < 9 && neighborY >= 0 && neighborY < 9) {
+                                                if (((BlockButton) buttons[neighborX][neighborY]).isEnabled() == true && ((BlockButton) buttons[neighborX][neighborY]).isFlag() == false) {
+                                                    ((BlockButton) buttons[neighborX][neighborY]).breakBlock();
+                                                    int mines = ((BlockButton) buttons[neighborX][neighborY]).getNeighborMines();
+                                                    if (mines == 0) {
+                                                        recursionBreak(neighborX, neighborY);
+                                                    }
+                                                }
+                                            }
+                                    }
+                                }
+                            });
 
             }
         }
